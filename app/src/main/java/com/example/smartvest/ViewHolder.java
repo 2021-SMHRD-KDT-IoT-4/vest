3ae5fa7eca2f869589fb443d9956193c8c7f9eab
smhrd <yyj951217@naver.com>
2021-08-17 18:19:54 +0900

    큐알 건듬

--- app/src/main/java/com/example/smartvest/ViewHolder.java
+++ app/src/main/java/com/example/smartvest/ViewHolder.java
@@ -4,7 +4,7 @@ import android.widget.TextView;
 
 public class ViewHolder {
 
-    TextView tv_company, tv_w_name, tv_work, tv_edu, tv_in_out;
+    TextView tv_w_name, tv_inout, tv_edu, tv_limit, tv_live;
     TextView tv_title;
     TextView tv_date;
 
